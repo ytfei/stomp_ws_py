d775d6b5c55aaf2bdcc8332b8fb6fa083be636fe
Mason <mason.p.yang@gmail.com>
2022-12-24 10:26:26 +0800

    test stomp

--- main.py
+++ main.py
@@ -14,19 +14,17 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT)
 
     # open transport
-    client = Client("ws://127.0.0.1:8080/gs-guide-websocket")
+    client = Client("ws://dev-option-websocket.mboxonline.com/optionWebsocket")
 
     # connect to the endpoint
-    client.connect(login="name",
-                   passcode="45C82C421EBA87C8131E220F878E4145",
-                   timeout=0)
+    client.connect(timeout=0)
 
     # subscribe channel
-    client.subscribe("/topic/1", callback=print_frame)
+    client.subscribe("/topic/mbp/btc", callback=print_frame)
 
     # send msg to channel
-    client.send("/topic/1", body=json.dumps({'name': 'tom'}))
+    # client.send("/topic/1", body=json.dumps({'name': 'tom'}))
 
-    time.sleep(3)
+    time.sleep(30)
 
     client.disconnect()
